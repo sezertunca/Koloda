6f7b178a1e6d2536b930d01d1f9d54892596a0cc
Sezer Tunca <sezertunca@gmail.com>
2021-06-27 15:07:00 +0100

    Fix dependency name

--- Package.swift
+++ Package.swift
@@ -22,7 +22,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "Koloda",
-            dependencies: ["pop"],
+            dependencies: ["pop-spm-package"],
             path: "Sources"),
     ]
 )

5dfbb3e5b5d6d8adb5fec84541a4ec4f61aa6d9c
Sezer Tunca <sezertunca@gmail.com>
2021-06-27 15:02:23 +0100

    Refactor

--- Package.swift
+++ Package.swift
@@ -4,12 +4,15 @@
 import PackageDescription
 
 let package = Package(
-    name: "Koloda",
+    name: "Kolado",
+        platforms: [
+        .iOS(.v13)
+    ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "Koloda",
-            targets: ["Koloda"]),
+            name: "Kolado",
+            targets: ["Kolado"]),
     ],
     dependencies: [
             .package(url: "https://github.com/sezertunca/pop-spm-package.git", from: "1.0.1"),
@@ -18,11 +21,8 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "Koloda",
-            dependencies: [ .product(name: "pop") ],
-            path: "Sources")
-        .testTarget(
-            name: "KolodaTests",
-            dependencies: ["Koloda"]),
+            name: "Kolado",
+            dependencies: ["pop"],
+            path: "Sources"),
     ]
 )
